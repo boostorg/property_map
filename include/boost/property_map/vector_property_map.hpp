57c91891fd5beac0720fa0636ed9dca556e5455c
Peter Dimov <pdimov@gmail.com>
2021-05-29 20:06:38 +0300

    Hide dependency on property_map_parallel

--- include/boost/property_map/vector_property_map.hpp
+++ include/boost/property_map/vector_property_map.hpp
@@ -94,7 +94,11 @@ namespace boost {
 }
 
 #ifdef BOOST_GRAPH_USE_MPI
-#include <boost/property_map/parallel/vector_property_map.hpp>
+// Hide include from dependency trackers; the right fix
+// is not to have it at all, but who knows what'll break
+#define BOOST_PMAP_HEADER_NAME <boost/property_map/parallel/vector_property_map.hpp>
+#include BOOST_PMAP_HEADER_NAME
+#undef BOOST_PMAP_HEADER_NAME
 #endif
 
 #endif
